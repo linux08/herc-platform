70b6c349aa33df4a5cf6f302bc447912391a8665
stackbaxter <stackbaxter@yahoo.com>
2019-02-21 02:59:23 -0600

    track flow, splash, track choice

---
+++ src/features/Track/TrackActionNames.js
@@ -0,0 +1,5 @@
+export const TrackActions = {
+    SetTransactions: 'SetTransactions',
+    SetLatestBlock: 'SetLatestBlock'
+
+}
---
+++ src/features/Track/screens/TrackSplash.js
@@ -0,0 +1,75 @@
+import { View, StatusBar, TouchableHighlight, FlatList } from "react-native";
+import styles from "../../assets/styles";
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { AssetCard } from "../../components/AssetCard";
+import {
+  ShowAssetPasswordModal,
+  SelectedAsset
+} from "../../features/SupplyChainFlow/Assets/AssetActionCreators";
+
+import AssetPasswordModal from "../../components/modals/AssetPasswordModal";
+
+// import { widthPercentageToDP, heightPercentageToDP } from '../../assets/responsiveUI';
+
+class TrackSplash extends Component {
+  constructor(props) {
+    super(props);
+    console.log("componentTest");
+  }
+
+  passwordCorrect = () => {
+    this.props.ShowAssetPasswordModal();
+    this.props.navigation.navigate("TrackSideChoice");
+  };
+
+
+  render() {
+    return (
+      <View style={styles.baseContainer}>
+        <StatusBar
+          barStyle={"light-content"}
+          translucent={true}
+          backgroundColor="transparent"
+        />
+        <View style={styles.bodyContainer}>
+          <FlatList
+            data={this.props.assets}
+            keyExtractor={item => item.Logo}
+            renderItem={item => {
+              return (
+                <TouchableHighlight
+                  key={item.item.Name}
+                  onPress={() => this.props.SelectedAsset(item.item)}
+                >
+                  <AssetCard asset={item.item} />
+                </TouchableHighlight>
+              );
+            }}
+          />
+
+        
+        </View>
+
+        <AssetPasswordModal
+          isVisible={this.props.showPasswordModal}
+          passwordCorrect={this.passwordCorrect}
+        />
+      </View>
+    );
+  }
+}
+
+const mapStateToProps = state => ({
+  assets: state.AssetReducers.assets,
+  selectedAsset: state.AssetReducers.selectedAsset,
+  showPasswordModal: state.AssetReducers.showPasswordModal
+});
+
+const mapDispatchToProps = dispatch => ({
+  ShowAssetPasswordModal: () => dispatch(ShowAssetPasswordModal()),
+  SelectedAsset: asset => dispatch(SelectedAsset(asset))
+});
+
+export default connect(mapStateToProps,mapDispatchToProps)(SupplyChainSplash);
+
