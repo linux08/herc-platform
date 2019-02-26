5ccb0a9db8f976806b98728d9dc89909dffbec74
julianapeace <chancecordelia@gmail.com>
2019-02-26 16:59:46 -0600

    built an apk from this commit 0.10.0

--- android/app/src/main/java/com/HERC/MainActivity.java
+++ android/app/src/main/java/com/HERC/MainActivity.java
@@ -2,9 +2,6 @@ package com.HERC;
 
 import com.facebook.react.ReactActivity;
 // import fr.greweb.reactnativeviewshot.RNViewShotPackage;
-import com.facebook.react.ReactActivityDelegate;
-import com.facebook.react.ReactRootView;
-// import com.swmansion.gesturehandler.react.RNGestureHandlerEnabledRootView;
 
 public class MainActivity extends ReactActivity {
 
@@ -16,14 +13,4 @@ public class MainActivity extends ReactActivity {
     protected String getMainComponentName() {
         return "HERC";
     }
-
-//     @Override
-//   protected ReactActivityDelegate createReactActivityDelegate() {
-//     return new ReactActivityDelegate(this, getMainComponentName()) {
-//       @Override
-//       protected ReactRootView createRootView() {
-//        return new RNGestureHandlerEnabledRootView(MainActivity.this);
-//       }
-//     };
-  }
-// }
+}
