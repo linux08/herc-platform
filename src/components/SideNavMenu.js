542dc8de8055684a9be45a9c715b6e0c5a5fcca2
julianapeace <chancecordelia@gmail.com>
2019-02-27 17:15:46 -0600

    fixed logout

--- src/components/SideNavMenu.js
+++ src/components/SideNavMenu.js
@@ -33,20 +33,20 @@ class SideMenu extends Component {
 
     }
 
-    // onLogOut = () => {
-    //     firebase.auth().signOut().then(function () {
-    //         // Sign-out successful.
-    //         console.log("Firebase Sign-out successful.")
-    //     }).catch(err => {
-    //         console.log(err)
-    //         // An error happened.
-    //     })
+    onLogOut = () => {
+        firebase.auth().signOut().then(function () {
+            // Sign-out successful.
+            console.log("Firebase Sign-out successful.")
+        }).catch(err => {
+            console.log(err)
+            // An error happened.
+        })
 
-    //     this.props.account.logout(function () {
-    //         console.log('hooray im out!')
-    //     })
-    //     this.props.navigation.navigate('Login');
-    // };
+        this.props.account.logout(function () {
+            console.log('hooray im out!')
+        })
+        this.props.navigation.navigate('Login');
+    };
 
 
     render() {
